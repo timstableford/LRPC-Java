fa97d67730c66aa99dce905e4b2a449aca951f36
Tim Stableford <tim.stableford@gmail.com>
2016-10-07 15:04:49 +0100

    Serializer cleanup

--- rpclib/src/main/java/uk/co/tstableford/rpclib/serializer/LSerializer.java
+++ rpclib/src/main/java/uk/co/tstableford/rpclib/serializer/LSerializer.java
@@ -66,11 +66,13 @@ public class LSerializer {
     }
 
     public void unserialize(ByteBuffer buffer) throws InvalidTypeException {
+        // Get the number of objects in the buffer.
         buffer.position(0);
         LObjects.RPCUInt8 size = new LObjects.RPCUInt8(0);
         size.parse(buffer, 0);
         this.data = new ArrayList<>((int)size.getData());
 
+        // For each object calculate how many bytes it uses and count the number of strings in the string table.
         int numStrings = 0;
         int sizes[] = new int[(int) size.getData()];
         for (int i = 0; i < size.getData(); i++) {
@@ -90,6 +92,8 @@ public class LSerializer {
 
         // Set to the start of the data section.
         int dataOffset = (int) (1 + size.getData() + numStrings);
+
+        // Parse each object in the buffer.
         for (int i = 0; i < size.getData(); i++) {
             LObjects.RPCUInt8 typeId = new LObjects.RPCUInt8(0);
             typeId.parse(buffer, 1 + i);
@@ -125,29 +129,38 @@ public class LSerializer {
             throw new InvalidTypeException("Too many data items!");
         }
 
+        // Allocate a buffer large enough to store the object.
         int dataSize = LType.UINT8.getSize(); // Number of items.
         dataSize += this.data.size() * LType.UINT8.getSize(); // One byte per item for identifier.
         dataSize += this.getStringOffset(this.data.size()) * LType.STRING.getSize(); // Get the number of strings. Multiple by the number of bytes used for string size.
         dataSize += this.getDataSize();
-
         ByteBuffer buffer = ByteBuffer.allocate(dataSize);
 
-        buffer.put(new LObjects.RPCUInt8(this.data.size()).getBytes().array(), 0, LType.UINT8.getSize());
+        // Put the number of items into the buffer.
+        putObject(buffer, LObjects.Int(LType.UINT8, this.data.size()));
+
+        // Put the object identifiers for each object into the buffer.
         for (LObject object: this.data) {
-            buffer.put(new LObjects.RPCUInt8(object.getType().getId()).getBytes().array(), 0, LType.UINT8.getSize());
+            putObject(buffer, LObjects.Int(LType.UINT8, object.getType().getId()));
         }
+
+        // Put the length of each string into the buffer.
         for (LObject object: this.data) {
             if (object.getType() == LType.STRING) {
-                buffer.put(new LObjects.RPCUInt8(object.getSize()).getBytes().array(), 0, LType.UINT8.getSize());
+                putObject(buffer, LObjects.Int(LType.UINT8, object.getSize()));
             }
         }
+
+        // Put the objects themselves into the buffer.
         for (LObject object: this.data) {
-            buffer.put(object.getBytes().array(), 0, object.getSize());
+            putObject(buffer, object);
         }
 
-        buffer.position(0);
-
         return buffer;
+    }
+
+    private void putObject(ByteBuffer buffer, LObject object) throws InvalidTypeException {
+        buffer.put(object.getBytes().array(), 0, object.getSize());
     }
 
     @Override
